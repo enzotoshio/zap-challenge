34a5c1fbd1c46c66e4f77ed4e759630df9238171
enzotoshio <enzo.toshiba@gmail.com>
2018-12-15 18:21:23 -0200

    adding property page style

--- src/container/property-page/index.js
+++ src/container/property-page/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import './style.css';
 import { getProperty } from '../../redux/properties/selectors';
 import { propertyType as propertyTypeEnum } from '../../enum/property';
 import { fetchProperty } from '../../redux/properties/actions';
+import Button from '../../component/button';
+import ThumbnailGallery from '../../component/thumbnail-gallery';
 
 class PropertyList extends Component {
   componentDidMount() {
@@ -18,24 +21,32 @@ class PropertyList extends Component {
         bedrooms,
         usableAreas,
         parkingSpaces,
+        images,
       },
+      history,
     } = this.props;
 
     return (
       <div className="app-container">
         <div className="property-details">
-          <p>
-            Apartamento para{' '}
-            {propertyTypeEnum[pricingInfos.businessType.toLowerCase()]}
-          </p>
-          <p>Banheiros: {bathrooms}</p>
-          <p>Quartos: {bedrooms}</p>
-          <p>
-            Área: {usableAreas}m<sup>2</sup>
-          </p>
-          <p>Vagas: {parkingSpaces}</p>
-          <p>R${pricingInfos.price}</p>
+          <div className="details">
+            <h2>
+              Apartamento para{' '}
+              {propertyTypeEnum[pricingInfos.businessType.toLowerCase()]}
+            </h2>
+            <p>Banheiros: {bathrooms}</p>
+            <p>Quartos: {bedrooms}</p>
+            <p>
+              Área: {usableAreas}m<sup>2</sup>
+            </p>
+            <p>Vagas: {parkingSpaces}</p>
+            <p>R${pricingInfos.price}</p>
+          </div>
+          <div className="gallery">
+            <ThumbnailGallery images={images} />
+          </div>
         </div>
+        <Button onClick={() => history.goBack()} text="Voltar" />
       </div>
     );
   }
@@ -77,7 +88,9 @@ const mapStateToProps = (state, props) => ({
   property: getProperty(state, { id: props.match.params.id }),
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PropertyList);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(PropertyList)
+);
