4eadb47ae8dc9a7bf85f900845950a5e956502c4
enzotoshio <enzo.toshiba@gmail.com>
2018-12-16 21:20:21 -0200

    adding business logic and its tests

--- src/service/__tests__/property.test.js
+++ src/service/__tests__/property.test.js
@@ -1,10 +1,27 @@
-import { getPropertyPublishers, isRental, isSale } from '../property';
+import {
+  getPropertyPublishers,
+  isRental,
+  isSale,
+  hasValidVivaMonthlyCondoFee,
+  hasMinimumZapSalePrice,
+  isValidLocation,
+  calculateUsableAreasPrice,
+  isInBoundBox,
+} from '../property';
 
 describe('property service', () => {
   describe('getPropertyPublishers', () => {
-    describe('when the property follows the rules for zap properties', () => {
+    describe('when the property follows the rental rules for zap properties', () => {
       it('returns a array with the correct publisher name', () => {
         const property = {
+          address: {
+            geoLocation: {
+              location: {
+                lon: -46.693419,
+                lat: -23.568704,
+              },
+            },
+          },
           pricingInfos: {
             price: '5000',
             businessType: 'RENTAL',
@@ -16,13 +33,28 @@ describe('property service', () => {
       });
     });
 
-    describe('when the property follows the rules for viva properties', () => {
+    describe('when the property follows the sale rules for zap properties', () => {
       it('returns a array with the correct publisher name', () => {
         const property = {
-          pricingInfos: {
-            price: '2000',
-            businessType: 'RENTAL',
+          address: {
+            geoLocation: { location: { lon: -46.693419, lat: -23.568704 } },
           },
+          usableAreas: '171',
+          pricingInfos: { price: '701000', businessType: 'SALE' },
+        };
+        const publishers = getPropertyPublishers(property);
+
+        expect(publishers).toEqual(['zap']);
+      });
+    });
+
+    describe('when the property follows the rental rules for viva properties', () => {
+      it('returns a array with the correct publisher name', () => {
+        const property = {
+          address: {
+            geoLocation: { location: { lon: -46.693419, lat: -23.568704 } },
+          },
+          pricingInfos: { price: '2000', businessType: 'RENTAL' },
         };
         const publishers = getPropertyPublishers(property);
 
@@ -30,10 +62,50 @@ describe('property service', () => {
       });
     });
 
-    describe('when the property follows the rules for viva and zap properties', () => {
+    describe('when the property follows the sale rules for viva properties', () => {
+      it('returns a array with the correct publisher name', () => {
+        const property = {
+          address: {
+            geoLocation: { location: { lon: -46.693419, lat: -23.568704 } },
+          },
+          pricingInfos: { price: '300000', businessType: 'SALE' },
+        };
+        const publishers = getPropertyPublishers(property);
+
+        expect(publishers).toEqual(['viva']);
+      });
+    });
+
+    describe('when the property follows the rental rules for viva and zap properties', () => {
       it('returns a array with the correct publishers names', () => {
         const property = {
-          pricingInfos: { price: '4000', businessType: 'RENTAL' },
+          address: {
+            geoLocation: { location: { lon: -43.0, lat: -23.568704 } },
+          },
+          pricingInfos: {
+            monthlyCondoFee: '100',
+            price: '3999',
+            businessType: 'RENTAL',
+          },
+        };
+        const publishers = getPropertyPublishers(property);
+
+        expect(publishers).toEqual(['zap', 'viva']);
+      });
+    });
+
+    describe('when the property follows the rental rules for viva and zap properties', () => {
+      it('returns a array with the correct publishers names', () => {
+        const property = {
+          address: {
+            geoLocation: { location: { lon: -43.0, lat: -23.568704 } },
+          },
+          usableAreas: 0,
+          pricingInfos: {
+            monthlyCondoFee: '100',
+            price: '600000',
+            businessType: 'SALE',
+          },
         };
         const publishers = getPropertyPublishers(property);
 
@@ -43,9 +115,7 @@ describe('property service', () => {
 
     describe('when the property doesnt follow the rules any publisher properties', () => {
       it('returns a array with the publisher "other"', () => {
-        const property = {
-          pricingInfos: { price: undefined, businessType: undefined },
-        };
+        const property = {};
         const publishers = getPropertyPublishers(property);
 
         expect(publishers).toEqual(['other']);
@@ -68,6 +138,77 @@ describe('property service', () => {
     it('returns true if businessType is equal to SALE', () => {
       const pricingInfo = { businessType: 'SALE' };
       const response = isSale(pricingInfo);
+
+      expect(response).toBeTruthy();
+    });
+  });
+
+  describe('hasValidVivaMonthlyCondoFee', () => {
+    it('returns true if the monthly condo fee is lower than 30% of the price', () => {
+      const payload = { monthlyCondoFee: '100', price: '3000' };
+      const response = hasValidVivaMonthlyCondoFee(payload);
+
+      expect(response).toBeTruthy();
+    });
+  });
+
+  describe('hasMinimumZapSalePrice', () => {
+    describe('when the property is inside the bound box', () => {
+      it('returns false if the monthly condo fee is lower than 600000', () => {
+        const payload = {
+          location: { lon: -46.693419, lat: -23.568704 },
+          price: '600000',
+        };
+        const response = hasMinimumZapSalePrice(payload);
+
+        expect(response).toBeFalsy();
+      });
+    });
+
+    describe('when the property is outside the bound box', () => {
+      it('returns true if the monthly condo fee is higher or equal to 600000', () => {
+        const payload = {
+          location: {},
+          price: '600000',
+        };
+        const response = hasMinimumZapSalePrice(payload);
+
+        expect(response).toBeTruthy();
+      });
+    });
+  });
+
+  describe('hasMaximumVivaRentalPrice', () => {
+    it('returns true if the price is lower than 4000', () => {
+      const payload = { location: {}, price: '3000' };
+      const response = hasValidVivaMonthlyCondoFee(payload);
+
+      expect(response).toBeTruthy();
+    });
+  });
+
+  describe('isValidLocation', () => {
+    it('returns true if lat and lon are not 0', () => {
+      const location = { lat: -23.546686, lon: -46.641146 };
+      const response = isValidLocation(location);
+
+      expect(response).toBeTruthy();
+    });
+  });
+
+  describe('calculateUsableAreasPrice', () => {
+    it('returns the correct price', () => {
+      const payload = { usableAreas: 10, price: 100 };
+      const response = calculateUsableAreasPrice(payload);
+
+      expect(response).toBe(10);
+    });
+  });
+
+  describe('isInBoundBox', () => {
+    it('returns true if the lat and lon are inside the allowed range', () => {
+      const location = { lat: -23.546686, lon: -46.641146 };
+      const response = isInBoundBox(location);
 
       expect(response).toBeTruthy();
     });
--- src/service/property.js
+++ src/service/property.js
@@ -1,23 +1,114 @@
-import { isEmpty } from 'lodash';
+import { isEmpty, inRange, defaultsDeep } from 'lodash';
 
 export const rules = {
   zap: [
-    ({ pricingInfos: { price, businessType } }) =>
-      isRental({ businessType }) && price >= 3500,
-    ({ pricingInfos: { price, businessType } }) =>
-      isSale({ businessType }) && price >= 600000,
+    ({
+      address: {
+        geoLocation: { location },
+      },
+      pricingInfos: { price, businessType },
+    }) =>
+      isValidLocation(location) && isRental({ businessType }) && price >= 3500,
+    ({
+      address: {
+        geoLocation: { location },
+      },
+      usableAreas,
+      pricingInfos: { price, businessType },
+    }) =>
+      isValidLocation(location) &&
+      isSale({ businessType }) &&
+      hasValidZapUsableAreas({
+        usableAreas,
+        price,
+      }) &&
+      hasMinimumZapSalePrice({
+        location,
+        price,
+      }),
   ],
   viva: [
-    ({ pricingInfos: { price, businessType } }) =>
-      isRental({ businessType }) && price <= 4000,
-    ({ pricingInfos: { price, businessType } }) =>
-      isSale({ businessType }) && price <= 700000,
+    ({
+      address: {
+        geoLocation: { location },
+      },
+      pricingInfos: { price, businessType, monthlyCondoFee },
+    }) =>
+      isValidLocation(location) &&
+      isRental({ businessType }) &&
+      hasMaximumVivaRentalPrice({ price, location }) &&
+      hasValidVivaMonthlyCondoFee({ price, monthlyCondoFee }),
+    ({
+      address: {
+        geoLocation: { location },
+      },
+      pricingInfos: { price, businessType },
+    }) =>
+      isValidLocation(location) && isSale({ businessType }) && price <= 700000,
   ],
 };
 
+export function hasValidVivaMonthlyCondoFee({ monthlyCondoFee, price }) {
+  const parsedMonthlyCondoFee = parseFloat(monthlyCondoFee);
+
+  if (isNaN(parsedMonthlyCondoFee) || parsedMonthlyCondoFee <= 0) return true;
+
+  const parsedPrice = parseFloat(price);
+
+  return monthlyCondoFee < parsedPrice * 0.3;
+}
+
+export function hasMinimumZapSalePrice({ location, price }) {
+  const parsedPrice = parseFloat(price);
+  const adjustedPrice = isInBoundBox(location)
+    ? parsedPrice * 0.9
+    : parsedPrice;
+
+  return adjustedPrice >= 600000;
+}
+
+export function hasMaximumVivaRentalPrice({ location, price }) {
+  const parsedPrice = parseFloat(price);
+  const adjustedPrice = isInBoundBox(location)
+    ? parsedPrice * 1.5
+    : parsedPrice;
+
+  return adjustedPrice <= 4000;
+}
+
+export function isValidLocation({ lat, lon }) {
+  return lat !== 0 && lon !== 0;
+}
+
+export function calculateUsableAreasPrice({ usableAreas, price }) {
+  return parseFloat(price) / usableAreas || 0;
+}
+
+export function hasValidZapUsableAreas({ usableAreas, price }) {
+  return (
+    usableAreas === 0 ||
+    calculateUsableAreasPrice({ usableAreas, price }) > 3500
+  );
+}
+
+export function isInBoundBox({ lat, lon }) {
+  return (
+    inRange(lat, -23.546685, -23.568704) && inRange(lon, -46.641145, -46.693419)
+  );
+}
+
 export function getPropertyPublishers(property) {
   const publishers = Object.keys(rules).reduce((acc, publisher) => {
-    const isFromPublisher = rules[publisher].some(rule => rule(property));
+    const isFromPublisher = rules[publisher].some(rule =>
+      rule(
+        defaultsDeep(property, {
+          address: {
+            geoLocation: { location: {} },
+          },
+          pricingInfos: {},
+        })
+      )
+    );
 
     if (isFromPublisher) acc.push(publisher);
 
